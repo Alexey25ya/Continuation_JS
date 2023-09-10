e981acfdbf8f194ba54a78aff5ea6e0e2113a358
Alexey <alexey25ya@mail.ru>
2023-09-10 21:19:51 +0300

    hw5

---
+++ HW5/script.js
@@ -0,0 +1,57 @@
+import { dataInfo } from "./zoo.js";
+
+const data = JSON.parse(dataInfo);
+console.log(data);
+const content = document.querySelector(".content");
+content.style.display = 'flex';
+content.style.flexWrap = 'wrap';
+content.style.justifyContent = 'center';
+
+data.forEach(({ id, animal, name, age, url }) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.style.border = '2px solid pink'
+    div.style.marginRight = '10px'
+    content.appendChild(div);
+
+    const img = document.createElement("img");
+    img.classList.add("card-top");
+    img.src = url;
+    img.width = 300;
+    img.height = 300;
+
+    div.appendChild(img);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    cardBody.style.display = 'flex';
+    cardBody.style.flexDirection = 'column';
+    cardBody.style.alignItems = "center";
+    div.appendChild(cardBody);
+
+    const idAnimal = document.createElement("p");
+    idAnimal.classList.add("card-text");
+    idAnimal.textContent = `id: ${id}`;
+    cardBody.appendChild(idAnimal);
+
+    const title = document.createElement("h4");
+    title.classList.add("card-subtitle");
+    title.textContent = animal;
+    title.style.fontSize = '20px';
+    title.style.color = 'green';
+    cardBody.appendChild(title);
+
+    const subtitleName = document.createElement("h5");
+    subtitleName.classList.add("card-title");
+    subtitleName.textContent = `Name: ${name}`;
+    subtitleName.style.color = 'Blue';
+    subtitleName.fontSize = '18px';
+    cardBody.appendChild(subtitleName);
+
+    const subtitle = document.createElement("h5");
+    subtitle.classList.add("card-subtitle");
+    subtitle.textContent = `Age: ${age}`;
+    cardBody.appendChild(subtitle);
+
+
+});
---
+++ HW5/zoo.js
@@ -0,0 +1,66 @@
+export const dataInfo = `[
+    {
+    "id": "1",
+    "animal": "Cat",
+    "name": "Kitty",
+    "age": "7",
+    "url": "https://static.life.ru/publications/2022/9/19/1161669205806.8777-600x.jpeg"
+    },
+    {
+    "id": "10",
+    "animal":"Dog",
+    "name": "Gav",
+    "age": "15",
+    "url": "https://static.sobaka.ru/images/image/01/63/21/50/_normal.jpeg?v=1671365310"
+    },
+    {
+    "id": "101",
+    "animal":"Fox",
+    "name": "Foxy",
+    "age": "10",
+    "url": "https://kartinkof.club/uploads/posts/2022-03/1648627341_1-kartinkof-club-p-smeshnaya-lisa-kartinki-1.jpg"
+    },
+    {
+    "id": "17",
+    "animal": "Lion",
+    "name": "Leo",
+    "age": "15",
+    "url": "https://i.ytimg.com/vi/DZGF2dVZ3WM/maxresdefault.jpg"
+    },
+    {
+    "id": "19",
+    "animal":"Elephant",
+    "name": "Hobot",
+    "age": "45",
+    "url": "https://kartinkof.club/uploads/posts/2022-03/1648669721_3-kartinkof-club-p-slon-smeshnie-kartinki-3.jpg"
+    },
+    {
+    "id": "75",
+    "animal":"Horse",
+    "name": "Spirit",
+    "age": "11",
+    "url": "https://kartinkof.club/uploads/posts/2022-03/1648626860_1-kartinkof-club-p-loshadi-smeshnie-kartinki-1.jpg"
+    }
+]
+    `;
+
+// export const dataInfo = `[
+//     {
+//     "id": "1",
+//     "name": "Vladimir",
+//     "age": "33",
+//     "url": "https://i.pinimg.com/564x/9e/0c/69/9e0c699ae56f0fb7be35b0c8778f79a0.jpg"
+//     },
+//     {
+//     "id": "10",
+//     "name": "Ivan",
+//     "age": "15",
+//     "url": "https://i.pinimg.com/564x/a1/b0/80/a1b0809e9d5ca159d2225d15681f03d7.jpg"
+//     },
+//     {
+//     "id": "101",
+//     "name": "Petr",
+//     "age": "51",
+//     "url": "https://i.pinimg.com/564x/a8/09/9f/a8099f0d240a3c6fd68eace46f7fd6ca.jpg"
+//     }]
+//     `;
